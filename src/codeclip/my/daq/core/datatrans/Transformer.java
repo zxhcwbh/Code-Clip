3b23d112c7026d0bce334055cd789dbc512f66c3
zxhcwbh <50574980+zxhcwbh@users.noreply.github.com>
2019-05-15 19:39:34 +0800

    Create Transformer.java

---
+++ src/codeclip/my/daq/core/datatrans/Transformer.java
@@ -0,0 +1,15 @@
+/**
+ * 文件名:Transformer.java
+ * 日期：2010-5-17
+ * @author：曾宪华
+ * @version:1.0
+ */
+
+package codeclip.my.daq.core.datatrans;
+
+/**
+ * 为数据转换定义统一的接口
+ */
+public interface Transformer {
+    public String transfer(String src);
+}
